201d9a45b0f0ad037c8828bc540df0b9492e567c
Francisco Sobrinho <francicsobrinho@gmail.com>
2022-11-11 22:54:22 -0300

    testing

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
-// ESM syntax is supported.
-export {}
+import { User } from "./db/models";
+import Site from "./db/models/site";
+
+(async () => {
+  try {
+
+    const joao = new User({
+      firstName: 'Joao',
+      lastName: 'Coriolano'
+    })
+    const site = new Site({ title: 'Shop' })
+
+
+    await joao.save()
+    await site.save()
+
+    await joao.setSite(site)
+
+    console.log(joao.get())
+
+    await joao.destroy()
+    await site.destroy()
+
+  } catch (error) {
+    console.log(error);
+  }
+
+})()
