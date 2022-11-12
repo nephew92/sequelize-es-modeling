f5f1bd34346f167dc2ef60b284226230718ff34f
Francisco Sobrinho <francicsobrinho@gmail.com>
2022-11-12 20:28:37 -0300

    test audit

--- src/index.js
+++ src/index.js
@@ -2,27 +2,34 @@ import { Site, User } from "./db/models";
 
 (async () => {
   try {
+    const site = await Site.create({ title: 'Shop' })
 
     const joao = await User.create({
       firstName: 'Joao',
-      lastName: 'Coriolano'
+      lastName: 'Coriolano',
     })
 
     const maria = await User.create({
       active: false,
       firstName: 'Maria',
-      lastName: 'Iraci'
+      lastName: 'Iraci',
+      siteId: site.id
     })
 
-    const site = await Site.create({ title: 'Shop' })
 
     await joao.setSite(site)
 
     {
-      const usersActive = await User.scope('limited', 'actives').findAll()
-      const usersSite = await User.scope('limited', 'withSites').findAll()
-      const users = await User.scope('limited').findAll()
-      console.log(JSON.stringify({ users, usersActive, usersSite }, null, 2))
+      // const usersActive = await User.scope('limited', 'actives').findAll()
+      // const usersSite = await User.scope('limited', 'withSites').findAll()
+      // const users = await User.scope('limited').findAll()
+      // console.log(JSON.stringify({ users, usersActive, usersSite }, null, 2))
+    }
+
+
+    {
+      const logs = await joao.getAuditLogs()
+      console.log(JSON.stringify({ site: await logs[0].getUserEntity(), logs: logs }, null, 2))
     }
 
 
